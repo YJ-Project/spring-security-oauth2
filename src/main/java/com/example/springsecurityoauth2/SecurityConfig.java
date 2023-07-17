fefc5d02d890d579ccf69c37928b2d0f874577cf
yujin <leeyujin95y@naver.com>
2023-07-17 17:02:37 +0900

    :sparkles: 11강 Http Basic 인증 - authenticationEntryPoint custom - 세션정책 테스트

--- src/main/java/com/example/springsecurityoauth2/SecurityConfig.java
+++ src/main/java/com/example/springsecurityoauth2/SecurityConfig.java
@@ -3,23 +3,18 @@ package com.example.springsecurityoauth2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain1(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests().anyRequest().authenticated();
-        http.formLogin();
-//        http.apply(new CustomSecurityConfigurer().setFlag(true));
-        return http.build();
-    }
-
-    @Bean
-    SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests().anyRequest().authenticated();
-        http.httpBasic();
+        http.httpBasic().authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); // 세션 값을 사용하지 않고, 생성도 하지 않음
         return http.build();
     }
 }
